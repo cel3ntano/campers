135c8029b6a3ffc0a277805bc4e5af362d77537f
cel3ntano <adriano2024celentano@gmail.com>
2024-11-27 01:26:03 +0100

    init: set up React app with initial configuration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Campers of your dreams</h1>
+      <p>You can find everything you want in our catalog</p>
+    </>
+  );
+}
+
+export default App;
